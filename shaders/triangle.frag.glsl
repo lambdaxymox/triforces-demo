fb03081961a947e00bcbb195082149c1ab726c06
LambdaXymox <lambda.xymox@gmail.com>
2018-07-18 23:19:38 -0500

    Remove old shaders.

--- shaders/triangle.frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 420 core
-
-out vec4 frag_color;
-
-
-void main() {
-    frag_color = vec4 (0.5, 0.0, 0.5, 1.0);
-}
-
--- shaders/triangle.vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 420 core
-
-in layout (location = 0) vec3 vp;
-
-
-void main() {
-    gl_Position = vec4(vp, 1.0);
-}
-
