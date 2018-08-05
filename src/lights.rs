c4b6fff2610134826bcc354938466b74883274ef
LambdaXymox <lambda.xymox@gmail.com>
2018-08-05 17:28:48 -0500

    Reorder arguments in receiver for PointLight constructor.

--- src/lights.rs
+++ src/lights.rs
@@ -2,18 +2,18 @@ use math::Vector3;
 
 
 pub struct PointLight {
-    pub specular: f32,
-    pub diffuse: f32,
     pub ambient: f32,
+    pub diffuse: f32,
+    pub specular: f32,
     pub position: Vector3,
 }
 
 impl PointLight {
-    pub fn new(specular: f32, diffuse: f32, ambient: f32, position: Vector3) -> PointLight {
+    pub fn new(ambient: f32, diffuse: f32, specular: f32, position: Vector3) -> PointLight {
         PointLight {
-            specular: specular,
-            diffuse: diffuse,
             ambient: ambient,
+            diffuse: diffuse,
+            specular: specular,
             position: position,
         }
     }
