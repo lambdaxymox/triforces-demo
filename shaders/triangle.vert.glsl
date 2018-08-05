8953badd4e5c41251752bf02ec9a4317ad9fd35f
LambdaXymox <lambda.xymox@gmail.com>
2018-08-04 22:53:32 -0500

    Refactor vertex shader to output all vertex parameters to eye space for coloring in the fragment stage.

--- shaders/triangle.vert.glsl
+++ shaders/triangle.vert.glsl
@@ -4,12 +4,14 @@ in layout (location = 0) vec3 v_pos;
 in layout (location = 1) vec2 v_tex;
 in layout (location = 2) vec3 v_norm;
 uniform mat4 proj_mat, view_mat, model_mat;
+out vec3 position_eye;
 out vec2 tex_coord;
-out vec3 normal;
+out vec3 normal_eye;
 
 
 void main() {
+    position_eye = vec3 (view_mat * model_mat * vec4 (v_pos, 1.0));
     tex_coord = v_tex;
-    normal = v_norm;
-    gl_Position = proj_mat * view_mat * model_mat * vec4 (v_pos, 1.0);
+    normal_eye = vec3 (view_mat * model_mat * vec4 (v_norm, 0.0));
+    gl_Position = proj_mat * vec4 (position_eye, 1.0);
 }
