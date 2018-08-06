7f4c8dd183eb12ab5f0e565f8d5789fdfefc1032
LambdaXymox <lambda.xymox@gmail.com>
2018-08-05 19:33:05 -0500

    Introduce diffuse term into blinn-phong model.

--- shaders/triangle.frag.glsl
+++ shaders/triangle.frag.glsl
@@ -4,6 +4,8 @@ in vec3 position_eye;
 in vec2 tex_coord;
 in vec3 normal_eye;
 
+uniform mat4 view_mat;
+uniform mat4 model_mat;
 uniform sampler2D tex;
 
 uniform PointLight {
@@ -18,11 +20,16 @@ out vec4 frag_color;
 
 
 void main() {
-    vec4 K = texture (tex, tex_coord);
-    vec3 n_eye = normalize (normal_eye);
-    vec3 Ia = vec3 (light.La * K);
+    vec3 K = vec3 (texture (tex, tex_coord));
+    vec3 norm_eye = normalize (normal_eye);
+    vec3 Ia = light.La * K;
 
-    vec3 Id = vec3(0.0, 0.0, 0.0);
+    vec3 light_pos_eye = vec3 (view_mat * vec4 (light.pos_wor, 1.0));
+    vec3 dist_to_light_eye = light_pos_eye - position_eye;
+    vec3 dir_to_light_eye = normalize (dist_to_light_eye);
+    float dot_diffuse = dot (dir_to_light_eye, norm_eye);
+    dot_diffuse = max (dot_diffuse, 0.0);
+    vec3 Id = light.Ld * K * dot_diffuse;
 
     vec3 Is = vec3(0.0, 0.0, 0.0);
     frag_color = vec4 (Ia + Id + Is, 1.0);
