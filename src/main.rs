cc63ee8fc416050c188de31dc82f822a9b17e82c
LambdaXymox <lambda.xymox@gmail.com>
2018-11-17 01:07:39 -0600

    Move load_image file to texture module.

--- src/main.rs
+++ src/main.rs
@@ -49,9 +49,6 @@ use std::process;
 use std::ptr;
 use std::collections::HashMap;
 
-use stb_image::image;
-use stb_image::image::LoadResult;
-
 
 // OpenGL extension constants.
 const GL_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FE;
@@ -128,49 +125,6 @@ fn create_camera(width: f32, height: f32) -> Camera {
     let axis = Quaternion::new(0.0, 0.0, 0.0, -1.0);
 
     Camera::new(near, far, fov, aspect, cam_speed, cam_yaw_speed, cam_pos, fwd, rgt, up, axis)
-}
-
-///
-/// Load texture image.
-///
-fn load_image<P: AsRef<Path>>(file_name: P) -> Result<TexImage2D, String> {
-    let force_channels = 4;
-    let mut image_data = match image::load_with_depth(&file_name, force_channels, false) {
-        LoadResult::ImageU8(image_data) => image_data,
-        LoadResult::Error(_) => {
-            let disp = file_name.as_ref().display();
-            return Err(format!("ERROR: could not load {}", disp));
-        }
-        LoadResult::ImageF32(_) => {
-            let disp = file_name.as_ref().display();
-            return Err(
-                format!("ERROR: Tried to load an image as byte vectors, got f32: {}", disp)
-            );
-        }
-    };
-
-    let width = image_data.width;
-    let height = image_data.height;
-
-    // Check that the image size is a power of two.
-    if (width & (width - 1)) != 0 || (height & (height - 1)) != 0 {
-        let disp = file_name.as_ref().display();
-        eprintln!("WARNING: texture {} is not power-of-2 dimensions", disp);
-    }
-
-    let width_in_bytes = 4 *width;
-    let half_height = height / 2;
-    for row in 0..half_height {
-        for col in 0..width_in_bytes {
-            let temp = image_data.data[row * width_in_bytes + col];
-            image_data.data[row * width_in_bytes + col] = image_data.data[((height - row - 1) * width_in_bytes) + col];
-            image_data.data[((height - row - 1) * width_in_bytes) + col] = temp;
-        }
-    }
-
-    let tex_image = TexImage2D::from(&image_data);
-
-    Ok(tex_image)
 }
 
 ///
@@ -314,7 +268,7 @@ fn create_ground_plane_geometry(context: &mut GameContext, id: EntityID) {
 }
 
 fn create_ground_plane_texture(context: &mut GameContext, id: EntityID) {
-    let tex_image = load_image(&context.asset_file("ground_plane.png")).unwrap();
+    let tex_image = texture::load_file(&context.asset_file("ground_plane.png")).unwrap();
     let tex = load_texture(&tex_image, gl::CLAMP_TO_EDGE).unwrap();
 
     context.entities.textures.insert(id, tex_image);
@@ -477,7 +431,7 @@ fn create_triforce_shaders(context: &mut GameContext, id: EntityID) {
 /// Load the triforce texture.
 ///
 fn create_triforce_texture(context: &mut GameContext, id: EntityID) {
-    let tex_image = load_image(&context.asset_file("triangle.png")).unwrap();
+    let tex_image = texture::load_file(&context.asset_file("triangle.png")).unwrap();
     let tex = load_texture(&tex_image, gl::CLAMP_TO_EDGE).unwrap();
 
     context.entities.textures.insert(id, tex_image);
--- src/texture.rs
+++ src/texture.rs
@@ -1,4 +1,6 @@
 use stb_image::image;
+use stb_image::image::LoadResult;
+use std::path::Path;
 
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -62,3 +64,45 @@ impl<'a> From<&'a image::Image<u8>> for TexImage2D {
     }
 }
 
+///
+/// Load a PNG texture image from a file.
+///
+pub fn load_file<P: AsRef<Path>>(file: P) -> Result<TexImage2D, String> {
+    let force_channels = 4;
+    let mut image_data = match image::load_with_depth(&file, force_channels, false) {
+        LoadResult::ImageU8(image_data) => image_data,
+        LoadResult::Error(_) => {
+            let disp = file.as_ref().display();
+            return Err(format!("ERROR: could not load {}", disp));
+        }
+        LoadResult::ImageF32(_) => {
+            let disp = file.as_ref().display();
+            return Err(
+                format!("ERROR: Tried to load an image as byte vectors, got f32: {}", disp)
+            );
+        }
+    };
+
+    let width = image_data.width;
+    let height = image_data.height;
+
+    // Check that the image size is a power of two.
+    if (width & (width - 1)) != 0 || (height & (height - 1)) != 0 {
+        let disp = file.as_ref().display();
+        eprintln!("WARNING: texture {} is not power-of-2 dimensions", disp);
+    }
+
+    let width_in_bytes = 4 *width;
+    let half_height = height / 2;
+    for row in 0..half_height {
+        for col in 0..width_in_bytes {
+            let temp = image_data.data[row * width_in_bytes + col];
+            image_data.data[row * width_in_bytes + col] = image_data.data[((height - row - 1) * width_in_bytes) + col];
+            image_data.data[((height - row - 1) * width_in_bytes) + col] = temp;
+        }
+    }
+
+    let tex_image = TexImage2D::from(&image_data);
+
+    Ok(tex_image)
+}
