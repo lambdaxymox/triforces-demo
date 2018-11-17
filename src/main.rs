0ed35e7d922c36f928af2f9572db4a661dd3416e
LambdaXymox <lambda.xymox@gmail.com>
2018-11-16 23:13:05 -0600

    Introduce dependencies for config module.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ extern crate glfw;
 extern crate stb_image;
 extern crate cgmath;
 extern crate wavefront;
+extern crate serde;
+extern crate toml;
 
+#[macro_use]
+extern crate serde_derive;
 
 mod gl {
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
