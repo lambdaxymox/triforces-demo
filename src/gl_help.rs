1ff27df6ee6568f9c90a4cc66a07f97d318b4d96
LambdaXymox <lambda.xymox@gmail.com>
2019-01-15 21:28:00 -0600

    Fix title screen name in startup code in helper module.

--- src/gl_help.rs
+++ src/gl_help.rs
@@ -216,7 +216,7 @@ pub fn start_gl(width: u32, height: u32) -> Result<GLState, String> {
 
     info!("Started GLFW successfully");
     let maybe_glfw_window = glfw.create_window(
-        width, height, &format!("Bitmapped Font Viewer"), glfw::WindowMode::Windowed
+        width, height, &format!("Triforces DEMO @ {} fps", 0.0), glfw::WindowMode::Windowed
     );
     let (mut window, events) = match maybe_glfw_window {
         Some(tuple) => tuple,
