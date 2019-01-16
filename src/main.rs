a90aa72cc0ae293bb4f2e1abb293aabdbf91dc5a
LambdaXymox <lambda.xymox@gmail.com>
2019-01-15 18:56:54 -0600

    Remove external dependencies that are no longer used.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,8 @@ extern crate glfw;
 extern crate stb_image;
 extern crate cgmath;
 extern crate wavefront;
-extern crate serde;
-extern crate toml;
 extern crate log;
 extern crate file_logger;
-
-#[macro_use]
-extern crate serde_derive;
 
 mod gl {
     include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
